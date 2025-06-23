5f7c78ab84a0d75c0fca0103e1b83021ba6706dc
Imran Khan <khanimran092606@gmail.com>
2025-06-23 15:55:52 -0400

    edited circle.java and created unit tests for it

--- Circle.java
+++ Circle.java
@@ -1,12 +1,12 @@
 package lab4;
-
 public class Circle {
 	Point centre;
 	double radius;
 	
-	public Circle(Point centre)
+	public Circle(Point centre, double radius)
 	{
 		this.centre = centre;
+		this.radius = radius;
 	}
 	public double diameter() {
 		return 2*radius;
