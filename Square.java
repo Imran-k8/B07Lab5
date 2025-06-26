2ea9febd01baa605ab1af215f06bbcde73de88d1
Langcheng He <1260407316@qq.com>
2025-06-25 22:17:26 -0400

    Add class Square

---
+++ Square.java
@@ -0,0 +1,22 @@
+package B07Lab5
+public class Square { 
+	double side;
+
+	public Square(double side) {
+	    
+	    this.side = side;
+	}
+	
+	public double getSide() {
+	    return side;
+	}
+	
+	public double getArea() {
+	    return side * side;
+	}
+	
+	public double getPerimeter() {
+	    return 4 * side;
+	}
+
+}
