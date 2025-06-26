39689e110c0233546f271788b061d34ed18e0e6f
Langcheng He <131352923+Langchenghe@users.noreply.github.com>
2025-06-25 22:51:55 -0400

    Update SquareTest.java

--- SquareTest.java
+++ SquareTest.java
@@ -6,23 +6,44 @@ import org.junit.jupiter.api.Test;
 class SquareTest {
 
     @Test
-    public void testGetSide() {
+    public void testGetSide1() {
     	double side = 5.0;
     	Square square = new Square(side);
         assertEquals(5.0, square.getSide());
     }
-
+    
     @Test
-    public void testGetArea() {
+    public void testGetSide2() {
+    	double side = 0.0;
+    	Square square = new Square(side);
+        assertEquals(0.0, square.getSide());
+    }
+    
+    @Test
+    public void testGetArea1() {
     	double side = 5.0;
     	Square square = new Square(side);
         assertEquals(25.0, square.getArea());
     }
 
     @Test
-    public void testGetPerimeter() {
+    public void testGetArea2() {
+    	double side = 0.0;
+    	Square square = new Square(side);
+        assertEquals(0.0, square.getArea());
+    }
+    
+    @Test
+    public void testGetPerimeter1() {
     	double side = 5.0;
     	Square square = new Square(side);
         assertEquals(20.0, square.getPerimeter());
+    }
+
+    @Test
+    public void testGetPerimeter2() {
+    	double side = 0.0;
+    	Square square = new Square(side);
+        assertEquals(0.0, square.getPerimeter());
     }
 }
