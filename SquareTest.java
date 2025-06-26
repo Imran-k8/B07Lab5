e110a026cd4cb91fc884f4da9aa92bffa6531303
Langcheng He <1260407316@qq.com>
2025-06-25 22:18:06 -0400

    Add JUnit test cases for class Square

---
+++ SquareTest.java
@@ -0,0 +1,28 @@
+package B07Lab5
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SquareTest {
+
+	@Test
+    public void testGetSide() {
+    	double side = 5.0;
+    	Square square = new Square(side);
+        assertEquals(5.0, square.getSide());
+    }
+
+    @Test
+    public void testGetArea() {
+    	double side = 5.0;
+    	Square square = new Square(side);
+        assertEquals(25.0, square.getArea());
+    }
+
+    @Test
+    public void testGetPerimeter() {
+    	double side = 5.0;
+    	Square square = new Square(side);
+        assertEquals(20.0, square.getPerimeter());
+    }
+}
