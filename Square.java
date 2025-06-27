6ad577b8606ed0269d43fa66fd61aa5e2ae017d6
Langcheng He <1260407316@qq.com>
2025-06-26 20:47:15 -0400

    Added SquareTest

---
+++ Square.java
@@ -0,0 +1,22 @@
+
+public class Square { 
+	double side;
+
+	public Square(double side) {
+	    
+	    this.side = side;
+	}
+	
+	public double getSide() {
+	    return side;
+	}
+	
+	public double getArea() {
+	    return side * side;
+	}
+	
+	public double getPerimeter() {
+	    return 4 * side;
+	}
+
+}
